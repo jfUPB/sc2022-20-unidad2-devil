e97f9338def1a89a56000b3dc090c032b57c05ac
DevilQu3enn <109630035+DevilQu3enn@users.noreply.github.com>
2022-10-31 01:36:56 -0500

    Update event.c

--- problem1/event.c
+++ problem1/event.c
@@ -4,10 +4,14 @@
 
 Event *CreateEvent(char *name)
 {
-
+  Event *event = malloc(sizeof(Event));
+  memcpy(event->eventName, name,15);
+  event->next=NULL;
+  
+  return event;
 }
 
 void DestroyEvent(Event *this)
 {
-
+  free(this);
 }
